2efd057cfb3fe1454e54eda727592cbfa3fb6946
Hossein Soltanloo <hossein.soltanloo@gmail.com>
2025-02-10 00:40:20 +0330

    Add CardComponent with styles for card layout

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style src="../assets/styles/card.css"></style>
